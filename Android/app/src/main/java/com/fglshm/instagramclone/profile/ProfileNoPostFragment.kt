340b66ae22dc292d5ee5e8118af3fe8d1a1d47c7
前野翔平 <fglshm@gmail.com>
2019-07-06 11:22:51 +0900

    rename file

--- Android/app/src/main/java/com/fglshm/instagramclone/profile/ProfileNoPostFragment.kt
+++ Android/app/src/main/java/com/fglshm/instagramclone/profile/ProfileNoPostFragment.kt
@@ -3,9 +3,9 @@ package com.fglshm.instagramclone.profile
 import com.fglshm.instagramclone.R
 import com.fglshm.instagramclone.base.BaseFragment
 
-class NoPostFragment : BaseFragment() {
+class ProfileNoPostFragment : BaseFragment() {
 
-    override val logTag: String = NoPostFragment::class.java.simpleName
+    override val logTag: String = ProfileNoPostFragment::class.java.simpleName
     override fun getLayout(): Int = R.layout.fragment_profile_no_post
 
 }
